f299d26d4d65fa553ff62e88af392a01f8c3d226
ChaEilish <Cogan@macbook-air-de-cogan.home>
2018-12-07 13:26:33 +0100

    maj

--- src/app/connexion/champselect/champselect.component.ts
+++ src/app/connexion/champselect/champselect.component.ts
@@ -11,9 +11,9 @@ export class ChampselectComponent{
   public herosName;
 
   constructor(){
-    this.heros_list.push(["../../../assets/img/heros1_ex.png",1,'Mage']);
-    this.heros_list.push(["../../../assets/img/heros2_ex.png",2,'Paladin']);
-    this.heros_list.push(["../../../assets/img/heros3_ex.png",3,'Mage']);
+    this.heros_list.push(["../../../assets/img/heros1_ex.png","hero1",'Mage',"Le Mage : Son action spéciale est la Boule de feu, infligeant un point de dégat à l'adversaire"]);
+    this.heros_list.push(["../../../assets/img/heros2_ex.png","hero2",'Paladin',"Le Paladin : Son action spéciale est le Renfort, en invoquant un serviteur"]);
+    this.heros_list.push(["../../../assets/img/heros3_ex.png","hero3",'Mage',"Le Guerrier : Son action spéciale est l'armure, lui conférant 2 points d'armure "]);
   }
 
   selected(heros){
